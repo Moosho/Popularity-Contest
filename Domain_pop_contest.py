fc7b2c325723d5dc13616b0b15250a3cd0eb8af7
Moosho <31143701+Moosho@users.noreply.github.com>
2018-01-20 17:42:01 +0100

    Changed class name to lowercase.

--- Domain_pop_contest.py
+++ Domain_pop_contest.py
@@ -121,11 +121,9 @@ class Website():
 
 
 print("Working...")
-# w = Website(3, domain="http://www.apl.com")
-# w = Website(3, domain="http://www.uyt.com")
-w = Website(5, "com",)
+w = Website(3, domain="http://www.tdeecalculator.net")
+# w = Website(5, "com",)
 w.start()
-print("Obj Website has been initialized.")
 print(w.getExternalLinks())
 print(w.getDomain())
 # print(w.getHtml())
