6b3334bb6d62f4b2ef31e92eb528c62867512a1d
Moosho <31143701+Moosho@users.noreply.github.com>
2018-01-20 19:02:00 +0100

    All works. Improved the script.

--- Domain_pop_contest.py
+++ Domain_pop_contest.py
@@ -125,13 +125,23 @@ class website():
 
 
 # Stript for testing
+list_of_them_all = []
 print("Working...")
-w = website(domain="http://www.apl.com")
+w = website()
 w.start()
 ex = w.getExternalLinks()
+list_of_them_all += ex
 print("This is {} domain.\nAnd these are it's external links: {}".format(w.getDomain(), ex))
 for x in ex:
     w = website(domain=x)
     w.start()
     ex = w.getExternalLinks()
+    list_of_them_all += ex
     print("This is {} domain.\nAnd these are it's external links: {}".format(w.getDomain(), ex))
+    for x in ex:
+        w = website(domain=x)
+        w.start()
+        ex = w.getExternalLinks()
+        list_of_them_all += ex
+        print("This is {} domain.\nAnd these are it's external links: {}".format(w.getDomain(), ex))
+print("\nAll external links found:\n{}".format(list_of_them_all))
