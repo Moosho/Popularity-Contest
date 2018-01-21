bd7a0571fdc8b2ff42ca3c507508f6f6910a5b08
Moosho <31143701+Moosho@users.noreply.github.com>
2018-01-21 10:45:06 +0100

    Added basic score mehanism to the scoreboard.

--- scoreboard.py
+++ scoreboard.py
@@ -1,8 +1,19 @@
+from collections import Counter
+
+
 class scoreboard():
     """Scores values given to it based on theyr frequency."""
 
     def __init__(self, *arg):
         self.urls = []
+        self.scored = None
         for x in arg:
             self.urls += x
         pass
+
+    def score(self,):
+        c = Counter()
+        for x in self.urls:
+            c[x] += 1
+        self.scored = c.most_common()
+        return self.scored
