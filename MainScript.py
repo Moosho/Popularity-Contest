78820e90a235e7ed59e53414175262c2570a78f5
Moosho <31143701+Moosho@users.noreply.github.com>
2018-01-21 11:23:13 +0100

    Improved script. It's working alright.

--- MainScript.py
+++ MainScript.py
@@ -3,23 +3,26 @@ from scoreboard import scoreboard
 
 list_of_them_all = []
 print("Working...")
-w = website()
+w = website(2, guesses=1000)
 w.start()
 ex = w.getExternalLinks()
 list_of_them_all += ex
-print("This is {} domain.\nAnd these are it's external links: {}".format(w.getDomain(), ex))
+# print("Layer 1. This is {} domain.\nAnd these are it's external links: {}".format(w.getDomain(), ex))
 for x in ex:
     w = website(domain=x)
     w.start()
     ex = w.getExternalLinks()
     list_of_them_all += ex
-    print("This is {} domain.\nAnd these are it's external links: {}".format(w.getDomain(), ex))
+    # print("Layer 2. This is {} domain.\nAnd these are it's external links: {}".format(w.getDomain(), ex))
     for x in ex:
         w = website(domain=x)
         w.start()
         ex = w.getExternalLinks()
         list_of_them_all += ex
-        print("This is {} domain.\nAnd these are it's external links: {}".format(w.getDomain(), ex))
-print("\nAll external links found:\n{}".format(list_of_them_all))
+        # print("Layer 3. This is {} domain.\nAnd these are it's external links: {}".format(w.getDomain(), ex))
+# print("\nAll external links found:\n{}".format(list_of_them_all))
 
 sb = scoreboard(list_of_them_all)
+# print(sb.score())
+print("\n")
+sb.show()
