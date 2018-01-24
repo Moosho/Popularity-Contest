9c2c905cad6c3b47101cf661e74efd1155602593
Moosho <31143701+Moosho@users.noreply.github.com>
2018-01-24 12:14:00 +0100

    Implemented Threading

--- MainScript.py
+++ MainScript.py
@@ -1,23 +1,35 @@
 from website import website
 from scoreboard import scoreboard
+from queue import Queue
 import threading
 
 exlGlobal = []
 tlist = []
 lock = threading.Lock()
+q = Queue()
 threads = 5
 
 
 def gel(web):
     global exlGlobal
+    with lock:
+        print(web)
     w = website(domain=web)
     w.start()
     exlGlobal += w.getExternalLinks()
 
 
-print("Start of the program...")
+def threader():
+    while True:
+        if q.empty() is True:
+            break
+        domain = q.get()
+        gel(domain)
+        q.task_done()
 
-w = website(domain="http://www.vb.com")
+
+print("Start of the program...")
+w = website(length=3, guesses=50)
 w.start()
 exlGlobal += w.getExternalLinks()
 
@@ -27,14 +39,15 @@ else:
 
     print("Root domain: {}\nLinks form first random domain: {}".format(w.getDomain(), exlGlobal))
 
-    for x in range(len(exlGlobal)):
-        thread = threading.Thread(target=gel, args=(exlGlobal[x],))
-        with lock:
-            print(exlGlobal[x])
-        thread.daemon = True
-        thread.start()
-        thread.join()
+    for domain in range(len(exlGlobal)):
+        q.put(exlGlobal[domain])
 
+    for thread in range(5):
+        t = threading.Thread(target=threader)
+        t.deamon = True
+        t.start()
+
+    q.join()
     sb = scoreboard(exlGlobal)
     # print(sb.score())
     print("\n")
